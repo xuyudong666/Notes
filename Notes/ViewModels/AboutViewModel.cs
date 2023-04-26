2ed1ba066ddd336180ec2fff02b3c16fcc75f143
徐欲东 <xuyd22082203@q1.com>
2023-04-26 10:22:47 +0800

    添加项目文件。

---
+++ Notes/ViewModels/AboutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class AboutViewModel
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "";
+    public string Message => "欲东创建的简约笔记本";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
+}
